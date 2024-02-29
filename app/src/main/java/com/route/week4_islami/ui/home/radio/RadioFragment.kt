0ed0daa2847cbbd383598dae95de0b1c4e9590e7
DESKTOP-Q2BPRDF\NEXT <mariamyasser3092@gmail.com>
2024-02-29 07:05:51 +0200

    +Add Radios Apis

--- app/src/main/java/com/route/week4_islami/ui/home/radio/RadioFragment.kt
+++ app/src/main/java/com/route/week4_islami/ui/home/radio/RadioFragment.kt
@@ -31,6 +31,7 @@ class RadioFragment :Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewBinding.errorView.isVisible =false
         getRadiosItems()
     }
 
